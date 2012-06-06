e59da3615f661131cffea8d0c8acd4f0d6031522
Saul <saul@diezguerra.es>
2012-06-06 19:59:32 -0300

    Update list-1.py with spaces after commas

--- list-1.py
+++ list-1.py
@@ -40,7 +40,7 @@ def rotate_left3(nums):
   Given an array of ints length 3, return an array with the elements "rotated
   left" so {1, 2, 3} yields {2, 3, 1}.
   """
-  return [nums[1],nums[2],nums[0]]
+  return [nums[1], nums[2], nums[0]]
 
 def reverse3(nums):
   """
