dbeb93a3bf417dab84c3c1b332e300728849f6b6
Abdul Kawsar Tushar <tushar.kawsar@gmail.com>
2020-01-05 09:09:36 -0500

    Updated solution to string_match()

--- warmup-2.py
+++ warmup-2.py
@@ -98,3 +98,17 @@ def string_match(a, b):
       count += 1
       
   return count
+
+def string_match(a, b):
+  """
+  Given 2 strings, a and b, return the number of the positions where they 
+  contain the same length 2 substring. So "xxcaazz" and "xxbaaz" yields 3, 
+  since the "xx", "aa", and "az" substrings appear in the same place in 
+  both strings. 
+  """
+    count = 0
+    for i in range(min(len(a), len(b))-1):
+        print("i",i)
+        if a[i:i+2] == b[i:i+2]:
+            count += 1
+    return count
