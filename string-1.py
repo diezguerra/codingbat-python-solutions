d701b007e782de5ddd51136870cca9d2dbddbb27
luciferthedevil1507 <66934872+luciferthedevil1507@users.noreply.github.com>
2020-09-19 23:55:37 +0530

    Update string-1.py

--- string-1.py
+++ string-1.py
@@ -63,7 +63,11 @@ def combo_string(a, b):
   with the shorter string on the outside and the longer string on the inside. 
   The strings will not be the same length, but they may be empty (length 0). 
   """
-  return a+b+a if len(a)<len(b) else b+a+b
+  def combo_string(a, b):
+  if len(a) < len(b):
+    return a+b+a
+  else:
+    return b+a+b
 
 def non_start(a, b):
   """
